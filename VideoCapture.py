9913250b38cedf46f20d4b1e44c1f6a9df3d7291
Tanathip:) <88467996+madara1150@users.noreply.github.com>
2024-02-16 22:27:58 +0700

    create : นำโมดูลมาใช้ร่วมกับ project

---
+++ VideoCapture.py
@@ -0,0 +1,31 @@
+import cv2
+import time
+import numpy as np
+import HandTrackingModule as htm
+
+# ขนาดจอ
+WIDTHCAM = 1280
+HIGHTCAM = 720
+
+cap = cv2.VideoCapture(1)
+cap.set(3, WIDTHCAM)
+cap.set(4,HIGHTCAM)
+previousTime = 0
+
+# นำโมดูลมาใช้
+detector = htm.handDetector()
+
+while True:
+    success, img = cap.read()
+    img = detector.findHands(img)
+
+    # ทำ FPS
+    currentTime = time.time()
+    fps = 1/(currentTime-previousTime)
+    previousTime = currentTime
+
+    # สร้าง text ขึ้นมาแสดงบนจอ
+    cv2.putText(img, f'FPS: {int(fps)}', (40,50), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 0, 0), 3)
+
+    cv2.imshow("Img", img)
+    cv2.waitKey(1)
--- handTracker.py
+++ handTracker.py
@@ -1,6 +1,7 @@
 import cv2
 import mediapipe as mp
 
+# ทดสอบใช้กล้องภายในเครื่อง
 mp_drawing = mp.solutions.drawing_utils
 mp_drawing_styles = mp.solutions.drawing_styles
 mphands = mp.solutions.hands
