8427635ffcd9be356af9000be7192b10cd100450
Tanathip:) <88467996+madara1150@users.noreply.github.com>
2024-02-19 22:22:33 +0700

    add : image พื้นหลัง

--- VideoCapture.py
+++ VideoCapture.py
@@ -4,19 +4,22 @@ import numpy as np
 import HandTrackingModule as htm
 
 # ขนาดจอ
-WIDTHCAM = 1280
-HIGHTCAM = 720
+WIDTHCAM = 800
+HIGHTCAM = 600
 
 cap = cv2.VideoCapture(1)
 cap.set(3, WIDTHCAM)
 cap.set(4,HIGHTCAM)
 previousTime = 0
 
+imgBG = cv2.imread('image/bg.png')
+
 # นำโมดูลมาใช้
 detector = htm.handDetector()
 
 while True:
     success, img = cap.read()
+    
     img = detector.findHands(img)
 
     # ทำ FPS
@@ -26,6 +29,6 @@ while True:
 
     # สร้าง text ขึ้นมาแสดงบนจอ
     cv2.putText(img, f'FPS: {int(fps)}', (40,50), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 0, 0), 3)
-
+    cv2.imshow("test", imgBG)
     cv2.imshow("Img", img)
     cv2.waitKey(1)
